e24f5a995c6edadd1241aed1d100c8244cec6db2
Hilário Grossi de Oliveira <hilariogrossi@yahoo.com.br>
2024-04-25 10:20:32 -0300

    Atualização 25/04/2024

---
+++ TypeScript/modulo-02/boolean.js
@@ -0,0 +1,9 @@
+"use strict";
+let nomeVariavel = true;
+
+let tarefaConcluida = true;
+let tarefaPendente = false;
+
+console.log(tarefaConcluida);
+console.log();
+console.log(tarefaPendente);
---
+++ TypeScript/modulo-02/boolean.ts
@@ -0,0 +1,8 @@
+let nomeVariavel: boolean = true;
+
+let tarefaConcluida: boolean = true;
+let tarefaPendente: boolean = false;
+
+console.log(tarefaConcluida);
+console.log();
+console.log(tarefaPendente);
