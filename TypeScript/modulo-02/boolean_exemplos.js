eb908e998524dc248510aa1e05a796ef79a16824
Hilário Grossi de Oliveira <hilariogrossi@yahoo.com.br>
2024-04-30 10:46:53 -0300

    Atualização 30/04/2024

---
+++ TypeScript/modulo-02/boolean_exemplos.js
@@ -0,0 +1,18 @@
+"use strict";
+let tarefaConcluida_1 = true;
+let tarefaPendente_1 = false;
+
+console.log(tarefaConcluida_1);
+console.log(tarefaPendente_1);
+console.log();
+
+let concluido = false;
+
+if (concluido) {
+    console.log('Tarefa concluída com sucesso!');
+    
+} else {
+    console.log('Tarefa NÃO concluída!');
+    
+}
+console.log();
---
+++ TypeScript/modulo-02/boolean_exemplos.ts
@@ -0,0 +1,21 @@
+// Exemplo 1
+let tarefaConcluida_1: boolean = true;
+let tarefaPendente_1: boolean = false;
+
+console.log(tarefaConcluida_1);
+console.log(tarefaPendente_1);
+
+
+
+// Exemplo 2
+let concluido: boolean = false;
+
+if (concluido) {
+    console.log('Tarefa concluída com sucesso!');
+    
+} else {
+    console.log('Tarefa NÃO concluída!');
+
+}
+
+console.log();
