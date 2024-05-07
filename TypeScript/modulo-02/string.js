31118adc2479946e6bd1b697b250abca74f7855a
Hilário Grossi de Oliveira <hilariogrossi@yahoo.com.br>
2024-05-07 10:16:40 -0300

    Atualização 07/05/2024

---
+++ TypeScript/modulo-02/string.js
@@ -0,0 +1,11 @@
+"use strict";
+// let nomeVariavel: string = valor;
+let animal_1 = 'Panda';
+let animal_2 = "Elefante";
+console.log(animal_1);
+console.log(animal_2);
+
+let nomePessoa = 'Hilário Grossi de Oliveira';
+let nomeEmpresa = 'Acesse';
+console.log(`\nO ${nomePessoa} trabalha na empresa ${nomeEmpresa}!\n`);
+
---
+++ TypeScript/modulo-02/string.ts
@@ -0,0 +1,13 @@
+// let nomeVariavel: string = valor;
+
+let animal_1: string = 'Panda';
+let animal_2: string = "Elefante";
+
+console.log(animal_1);
+console.log(animal_2);
+
+
+let nomePessoa: string = 'Hilário Grossi de Oliveira';
+let nomeEmpresa: string = 'Acesse';
+
+console.log(`O ${nomePessoa} trabalha na empresa ${nomeEmpresa}!`);
