d4f3be93a73cc7021c95d4eef0c76d60ec7d2206
Hilário Grossi de Oliveira <hilariogrossi@yahoo.com.br>
2024-05-08 10:00:00 -0300

    Atualização 08/05/2024

---
+++ TypeScript/modulo-02/strings_demo.js
@@ -0,0 +1,15 @@
+"use strict";
+
+// Exemplo 01 - Single Quotes (Aspas simples)
+let nomeCompleto = 'Hilário Grossi de Oliviera';
+console.log(nomeCompleto);
+
+// Exemplo 02 - Double Quotes (Aspas duplas)
+let funcaoEmpresa = "Clound Advocate em JavaScript";
+console.log(funcaoEmpresa);
+
+// Exemplo 03 - Back Ticks (Acentos crase)
+let nomeEmpresa_1 = 'Acesse';
+let dadosFuncionario = `\nSeja bem vindo ${nomeCompleto}! Você será ${funcaoEmpresa} na empresa ${nomeEmpresa_1}.\n`;
+
+console.log(dadosFuncionario);
