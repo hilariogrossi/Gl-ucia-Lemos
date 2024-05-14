2330f424b04d1fd5129a2c4144adcde6053b6a0e
Hilário Grossi de Oliveira <hilariogrossi@yahoo.com.br>
2024-05-14 09:10:06 -0300

    Atualização 14/05/2024

---
+++ TypeScript/modulo-02/array.js
@@ -0,0 +1,15 @@
+"use strict";
+
+// let nomeVariavel: type[];
+
+// ==> Exemplo 1 - Uso de Colchetes
+let frutas_1 = ['abacaxi', 'laranja', 'maçã', 'melancia', 'manga'];
+console.log(frutas_1);
+
+console.log();
+
+// ==> Exemplo 2 - Array Object
+let frutas_2 = ['abacaxi', 'laranja', 'maçã', 'melancia', 'manga'];
+console.log(frutas_2);
+
+console.log();
---
+++ TypeScript/modulo-02/array.ts
@@ -0,0 +1,9 @@
+// let nomeVariavel: type[];
+
+// ==> Exemplo 1 - Uso de Colchetes
+let frutas_1: string[] = ['abacaxi', 'laranja', 'maçã', 'melancia', 'manga'];
+console.log(frutas_1);
+
+// ==> Exemplo 2 - Array Object
+let frutas_2: Array<string> = ['abacaxi', 'laranja', 'maçã', 'melancia', 'manga'];
+console.log(frutas_2);
