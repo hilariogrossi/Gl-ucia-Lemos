7ed64576f3f388ef50d72bfd900f60eda7c5e44f
Hilário Grossi de Oliveira <hilariogrossi@yahoo.com.br>
2024-05-28 19:46:40 -0300

    Atualização 28/05/2024  - 1

--- TypeScript/modulo-02/object_2.ts
+++ TypeScript/modulo-02/object_2.ts
@@ -62,13 +62,128 @@ console.log(onBoarding_03( { Nome: 'Hilário', Função: 'Programador', Linguage
 console.log();
 
 // ==> Exemplo 05: Usando optional no Object
+interface Pessoa_04 {
+    Nome: string;
+    Função: string;
+    Linguagem: string;
+    Email ?: string;
+
+}
+
+function onBoarding_04 (pessoa: Pessoa_04) {
+    return (
+
+        `Seja bem-vindo ${pessoa.Nome}! Sua função aqui na empresa é ${pessoa.Função}, usando a linguagem ${pessoa.Linguagem} e seu email é ${pessoa.Email}.`
+
+    );
+
+}
+
+console.log(onBoarding_04( { Nome: 'Hilário', Função: 'Programador', Linguagem: 'TypeScript', Email: 'hilariogrossi@yahoo.com.br' }));
+console.log();
 
 // ==> Exemplo 06: Propriedade 'readonly' (se deseja proibir que os devs não modifiquem um determinado
     // objeto use o readonly
+    interface Pessoa_05 {
+        Nome: string;
+        Função: string;
+        Linguagem: string;
+        readonly Email: string;
+    
+    }
+    
+    function onBoarding_05 (pessoa: Pessoa_05) {
+        return (
+    
+            `Seja bem-vindo ${pessoa.Nome}! Sua função aqui na empresa é ${pessoa.Função}, usando a linguagem ${pessoa.Linguagem} e seu email é ${pessoa.Email}.`
+    
+        );
+    
+    }
+    
+    console.log(onBoarding_05( { Nome: 'Hilário', Função: 'Programador', Linguagem: 'TypeScript', Email: 'hilariogrossi@yahoo.com.br' }));
+    console.log();
 
 // ==> Exemplo 07: Tipos de extensões (heranças)
+interface Mãe {
+    Nome: string;
+
+}
+
+interface Pai {
+    Sobrenome: string;
+
+}
+
+interface Filho extends Mãe, Pai {
+    Idade: number;
+
+}
+
+const filho: Filho = {
+    Nome: 'Hilário',
+    Sobrenome: 'Oliveira',
+    Idade: 53,
+
+}
+
+console.log(filho);
+console.log();
 
 // ==> Exemplo 08: Tipos de interseções
+interface Cachorro {
+    Tipo: string;
+
+}
+
+interface Gato {
+    Tipo: string;
+
+}
+
+type Animal = Cachorro & Gato;
 
 // ==> Exemplo 09: Generics Objects
+type Usuário = {
+    Nome: string;
+    Email: string;
 
+}
+
+type Admin = {
+    Nome: string;
+    Email: string;
+    Admin: boolean;
+
+}
+
+const usuário: Usuário = {
+    Nome: 'Hilário',
+    Email: 'hilariogrossi@yahoo.com.br',
+
+}
+
+const admin: Admin = {
+    Nome: 'Hilário',
+    Email: 'hilariogrossi@yahoo.com.br',
+    Admin: true,
+
+}
+
+
+function acessarSistema<T> (usuário: T) {
+    return usuário;
+
+}
+
+console.log(acessarSistema<Usuário>(usuário));
+console.log(acessarSistema<Admin>(admin));
+
+
+/*function acessarSistema (usuário: Usuário) {
+    return usuário;
+    
+}
+
+console.log(acessarSistema(usuário));
+console.log();*/
