828d0ef98f4747c81e831c4b396134c1fa0449ac
Hilário Grossi de Oliveira <hilariogrossi@yahoo.com.br>
2024-06-19 10:38:36 -0300

    Atualização 19/06/2024

--- TypeScript/modulo-05/modificadores_de_acesso_03.ts
+++ TypeScript/modulo-05/modificadores_de_acesso_03.ts
@@ -85,3 +85,82 @@ const estudande_03 = new Pessoa (321, 'João Carlos de Almeida', 'Matemática');
 console.log(estudande_03.returnarDadosAlunos());
 
 // ==> Exemplo 05 - Modificador: readonly
+class Funcionario_01 {
+    readonly dataNascimento: Date;
+
+    constructor (dataNascimento: Date) {
+        this.dataNascimento = dataNascimento;
+    
+    }
+
+}
+
+const funcionario_01 = new Funcionario_01 (new Date(1970, 10, 21));
+
+// funcionario.dataNascimento = new Date (1970, 10, 22); Cannot assign to 'dataNascimento' because it is a read-only property.
+
+console.log(funcionario_01);
+console.log();
+
+// ==> Exemplo 06 - Modificador: readonly
+class Funcionario_02 {
+    constructor (readonly dataNascimento: Date) {
+        this.dataNascimento = dataNascimento;
+    
+    }
+
+}
+
+const funcionario_02 = new Funcionario_02 (new Date(1970, 10, 21));
+
+console.log(funcionario_02);
+console.log();
+
+// ==> Exemplo 07 - Modificador: readonly
+class Funcionario_03 {
+    nome: string;
+    readonly codigoFuncionario: number;
+
+    constructor (nome: string, codigo: number) {
+        this.codigoFuncionario = codigo;
+        this.nome = nome;
+    
+    }
+
+}
+
+const funcionario_03 = new Funcionario_03 ('Hilário Grossi de Oliveira', 1);
+
+console.log(funcionario_03);
+console.log();
+
+// ==> Exemplo 07 - Modificador: readonly Interface
+interface IFuncionario {
+    codigoFuncionario: number;
+    nomeEmpregado: string;
+
+}
+
+const funcionario_04: Readonly<IFuncionario> = {
+    codigoFuncionario: 5,
+    nomeEmpregado: 'Thiago Grossi de Oliveira',
+
+};
+
+// funcionario_04.codigoFuncionario = 6; Cannot assign to 'codigoFuncionario' because it is a read-only property.
+// funcionario_04.nomeEmpregado = 'Gustavo Grossi de Oliveira'; Cannot assign to 'codigoFuncionario' because it is a read-only property.
+
+console.log(funcionario_04);
+console.log();
+
+const funcionario_05: IFuncionario = {
+    codigoFuncionario: 5,
+    nomeEmpregado: 'Thiago Grossi de Oliveira',
+
+};
+
+funcionario_05.codigoFuncionario = 6;
+funcionario_05.nomeEmpregado = 'Gustavo Grossi de Oliveira';
+
+console.log(funcionario_05);
+console.log();
